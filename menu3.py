ad229174f3eb5311c368bb8309284165baa8ea4b
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2020-01-09 17:43:29 -0500

    se agrego el tercer menu al python file menu3.py

---
+++ menu3.py
@@ -0,0 +1,29 @@
+import libreria
+
+def precioguitarra():
+    a = libreria.pedir_numero("ingrese el precio de la guitarra: ", 0, 500)
+    print("el precio es: " + str(a) + " " + "$")
+
+def preciobateria():
+    b = libreria.pedir_numero("ingrese el precio de la bateria: ", 0, 500)
+    print("el precio de la bateria es: " + str(b) + " " + "$")
+
+opc = 0
+final = 3
+while (opc != final):
+    print("=====================================")
+    print("1. GUITARRA: el precio de la guitarra")
+    print("2. BATERIA: el precio de la bateria")
+    print("3. SALIR: fin del programa")
+    print("=====================================")
+
+    opc = libreria.pedir_numero("ingrese la opcion : ", 1, 3)
+
+    if (opc == 1):
+        precioguitarra()
+
+    if (opc == 2):
+        preciobateria()
+
+# fin_menu
+print("Fin Del Programa")
