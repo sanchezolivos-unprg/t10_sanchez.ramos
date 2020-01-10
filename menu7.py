ef991af30fe8922399349fa7561e5242ead15901
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2020-01-09 19:19:40 -0500

    se agrego el septimo menu al python file menu7.py

---
+++ menu7.py
@@ -0,0 +1,42 @@
+import libreria
+
+# el precio de los celulares ultima generacion del 2019
+def precio_samsung():
+    print("el precio del celular samsung ultima generacion es de 1150 soles")
+
+def precio_huawei():
+    print("el precio del celular huawei ultima generacion es de 1200 soles")
+
+def precio_iphone():
+    print("el precio del celular iphone ultima generacion es de 2500 soles")
+
+def precio_motorola():
+    print("el precio del celular motorola ultima generacion es de 910 soles")
+
+opc = 0
+end = 5
+while (opc != end):
+    print("############# MARCAS DE CELULARES ##############")
+    print("1. SAMSUNG: PRECIO ")
+    print("2. HUAWEI: PRECIO ")
+    print("3. IPHONE: PRECIO ")
+    print("4. MOTOROLA: PRECIO ")
+    print("5. SALIR: end of the program")
+    print("################################################")
+
+    opc = libreria.pedir_numero("ingrese la opcion: ", 1, 5)
+
+    if (opc == 1):
+        precio_samsung()
+
+    if (opc == 2):
+        precio_huawei()
+
+    if (opc == 3):
+        precio_iphone()
+
+    if (opc == 4):
+        precio_motorola()
+
+# fin_menu
+print("fin del programa")
