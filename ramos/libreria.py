c19233f39e414e0340c2f1ad725520116e02d499
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2020-01-10 01:56:21 -0500

    se agrego el la libreria de mi compñero al directory ramos

---
+++ ramos/libreria.py
@@ -0,0 +1,55 @@
+#VALIDAR SI UN NUMERO ES ENTERO
+
+def validar_entero(n):
+    if (isinstance(n, int)):
+        return True
+    else:
+        return False
+#VALIDAR SI UN NUMERO ESTA ENTRE UN RANGO VALIDO
+def validar_rango(n, ri, rf):
+    if ( validar_entero(n) == True):
+        if (n >= ri and n <= rf):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def pedir_numero(msg, ri, rf):
+    n=""
+    while( validar_rango(n, ri, rf) == False):
+       n=input(msg)
+       if ( n.isdigit() == True):
+           n = int(n)
+    #fin_while
+    return n
+#fin_pedir_numero
+
+def validar_nombre(nombre):
+    if ( isinstance(nombre, str)):
+        if (len(nombre) >= 3):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def pedir_nombre(msg):
+    nombre=""
+    while ( validar_nombre(nombre) == False ):
+        nombre=input(msg)
+    #fin_while
+    return nombre
+
+#fin_pedir_nombre
+
+def guardar_datos(nombre_archivo, contenido, modo):
+    archivo=open(nombre_archivo, modo)
+    archivo.write(contenido)
+    archivo.close()
+
+def obtener_datos(nombre_archivo):
+    archivo=open(nombre_archivo)
+    contenido = archivo.read()
+    archivo.close()
+    return contenido
